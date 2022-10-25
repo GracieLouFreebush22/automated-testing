0dd70d863c40149f012385128fc3e1ec7a252665
grace k <grace.kuperman5472@gmail.com>
2022-10-25 14:12:22 +0100

    working now short test

--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -1,6 +1,7 @@
 const findShortestString = require("./find-shortest--copy-string");
 
 test("findShortestString finds the shortest string in an array", () => {
-    expect(findShortestString(["a", "cat"])).toBe("a");
-    //expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
-  });
+  expect(findShortestString(["a", "cat"])).toBe("a");
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
+  expect(findShortestString(["a", "dog", "went", "outisde"])).toBe("a");
+});
